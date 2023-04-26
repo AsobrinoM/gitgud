3d8353ae4a8a5389bb242dcf7c937af5468f9a49
Asobrino <asobrinom16@educastillalamancha.es>
2023-04-26 10:09:10 +0200

    Tercer commit, Antonio sobrino

--- src/gitgud/FUtil.java
+++ src/gitgud/FUtil.java
@@ -6,12 +6,10 @@ public class FUtil {
 	public static boolean existe(String rutaArchivo) {
 		File archivo =new File(rutaArchivo);
 		return archivo.exists();
+	}
+	public void escribo (String a_escribir) {
 		
-		
-	
-			
-	
-	
+		System.out.println(a_escribir);
 	}
 	}
 
